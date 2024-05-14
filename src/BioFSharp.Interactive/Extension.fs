5de60b6cb3e57ef4ea3870f03365d01b208b58db
Kevin Schneider <schneider.kev@outlook.de>
2024-05-14 09:29:47 +0200

    port biocollections docs, expose formatter registration in BioFSharp.Interactive

--- src/BioFSharp.Interactive/Extension.fs
+++ src/BioFSharp.Interactive/Extension.fs
@@ -11,18 +11,5 @@ type FormatterKernelExtension() =
 
     interface IKernelExtension with
         member _.OnLoadAsync _ =
-            Formatter.Register<AminoAcids.AminoAcid>(
-                Action<_, _>
-                    (fun item (writer: IO.TextWriter) ->
-                        let pretty = FSIPrinters.prettyPrintBioItem item
-                        writer.Write(pretty)),
-                "text/html"
-            )
-            Formatter.Register<seq<IBioItem>>(
-                Action<_, _>
-                    (fun bioCollection (writer: IO.TextWriter) ->
-                        let pretty = $"<pre>{FSIPrinters.prettyPrintBioCollection bioCollection}</pre>"
-                        writer.Write(pretty)),
-                "text/html"
-            )
+            Formatters.registerAll()
             Task.CompletedTask
