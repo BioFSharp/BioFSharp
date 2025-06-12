07957279807e7c8b9a92c33848456d0703489402
vLeidel <leidel@rhrk.uni-kl.de>
2025-06-12 18:41:00 +0200

    add BioFSharp.Benchmarks project

---
+++ benchmarks/BioFSharp.Benchmarks/config/config.fs
@@ -0,0 +1,22 @@
+﻿module BioFSharp.Benchmarks.Config
+
+open BenchmarkDotNet.Configs
+open BenchmarkDotNet.Exporters
+open BenchmarkDotNet.Exporters.Csv
+open BenchmarkDotNet.Loggers
+
+let createConfig (argv: string[]) =
+    let folderName =
+        argv
+        |> Array.toList
+        |> function
+            | "--filter" :: value :: _ -> value
+            | value :: _ when value.StartsWith("--filter=") -> value.Split('=').[1]
+            | _ -> "AllBenchmarks"
+        |> fun raw ->
+            raw.Replace("*", "").Replace("+", "_").Replace(".", "_").Trim()
+
+    ManualConfig
+        .Create(DefaultConfig.Instance)
+        .WithArtifactsPath($"BenchmarkDotNet.Artifacts/{folderName}")
+        
---
+++ benchmarks/BioFSharp.Benchmarks/main.fs
@@ -0,0 +1,18 @@
+﻿module Main
+
+open BenchmarkDotNet.Running
+open System.Reflection
+open BioFSharp.Benchmarks.Config
+
+
+
+[<EntryPoint>]
+let main argv =
+    let config = createConfig argv
+    BenchmarkSwitcher
+        .FromAssembly(Assembly.GetExecutingAssembly())
+        .Run(argv, config)
+        |> ignore
+
+    0 // return an integer exit code
+
