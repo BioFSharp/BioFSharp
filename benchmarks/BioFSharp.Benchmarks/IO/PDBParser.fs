46855bc2cf847585f476c0e96bb8cc412c41d41a
vLeidel <leidel@rhrk.uni-kl.de>
2025-06-12 18:41:00 +0200

    add BioFSharp.Benchmarks project

---
+++ benchmarks/BioFSharp.Benchmarks/IO/PDBParser.fs
@@ -0,0 +1,108 @@
+﻿namespace BioFSharp.Benchmarks.IO
+
+module PDBParserBenchmarks =
+    open BenchmarkDotNet.Attributes
+    open BioFSharp.IO.PDBParser
+
+
+    let testdata = "resources/rubisCOActivase.pdb"
+    let htq = "resources/htq.pdb"
+
+    [<MemoryDiagnoser>]
+    type pdbParserBenchmarks() =
+               
+        [<Benchmark>]
+        member this.ReadPDBFileNormal() =
+            let lines = readPBDFile testdata
+            lines |> Seq.length
+
+        [<Benchmark>]
+        member this.ReadPDBFileExtreme() =
+            let lines = readPBDFile htq
+            lines |> Seq.length
+
+         [<Benchmark>]
+            member this.readMetadataNormal() =
+                let lines = readMetadata (readPBDFile testdata)
+                lines 
+
+        [<Benchmark>]
+        member this.readMetadataExtreme() =
+            let lines = readMetadata (readPBDFile htq)
+            lines 
+
+        [<Benchmark>]
+        member this.readAtomNormal() =
+            let lines = readAtom (readPBDFile testdata)
+            lines 
+
+        [<Benchmark>]
+        member this.readAtomExtreme() =
+            let lines = readAtom (readPBDFile htq)
+            lines 
+
+       
+        [<Benchmark>]
+        member this.readResidueNormal() =
+            let lines = readResidues (readPBDFile testdata)
+            lines 
+
+        [<Benchmark>]
+        member this.readResidueExtreme() =
+            let lines = readResidues (readPBDFile htq)
+            lines 
+
+        [<Benchmark>]
+        member this.readChainNormal() =
+            let lines = readChain (readPBDFile testdata)
+            lines 
+
+        [<Benchmark>]
+        member this.readChainExtreme() =
+            let lines = readChain (readPBDFile htq)
+            lines 
+
+        [<Benchmark>]
+        member this.readLinkageNormal() =
+            let lines = readLinkages (readPBDFile testdata)
+            lines 
+
+        [<Benchmark>]
+        member this.readLinkageExtreme() =
+            let lines = readLinkages (readPBDFile htq)
+            lines 
+
+    
+        [<Benchmark>]
+        member this.readSiteNormal() =
+            let lines = readSite (readPBDFile testdata)
+            lines 
+
+        [<Benchmark>]
+        member this.readSiteExtreme() =
+            let lines = readSite (readPBDFile htq)
+            lines 
+
+        [<Benchmark>]
+        member this.readModelNormal() =
+            let lines = readModels (readPBDFile testdata)
+            lines 
+      
+        [<Benchmark>]
+        member this.readModel_extreme() =
+            let lines = readModels (readPBDFile htq)
+            lines
+
+        [<Benchmark>]
+        member this.readStructureNormal() =
+            let lines = readStructure  testdata
+            lines 
+
+        [<Benchmark>]
+        member this.readStructureHigh() =
+            let lines = readStructure  htq
+            lines 
+
+
+  
+
---
+++ benchmarks/BioFSharp.Benchmarks/main.fs
@@ -0,0 +1,11 @@
+﻿module Main
+
+open BenchmarkDotNet.Running
+
+open BioFSharp.Benchmarks.IO
+
+[<EntryPoint>]
+let main argv =
+    BenchmarkRunner.Run<PDBParserBenchmarks.pdbParserBenchmarks>() |> ignore
+    0
+    
